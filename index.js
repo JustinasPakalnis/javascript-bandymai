abcb21863fa8ba110a85991fdc57e52e5b55c2fc
JustinasPakalnis <justinas.p@inbox.lt>
2024-07-09 19:15:49 +0300

    meh

--- index.js
+++ index.js
@@ -264,26 +264,7 @@
 
 
 
-// var output = [];
 
-// function fizzBuzz (){
-//     for (var count=1; count < 101; count++){
-//     if (count % 3 === 0 && count % 5 === 0) {
-//         output.push ('FizzBuzz');
-//     }
-//     else if (count % 3 === 0) {
-//         output.push ('Fizz');
-//     }
-//     else if (count % 5 === 0) {
-//         output.push ('buzz');
-//     }
-//     else {
-//         output.push (count);
-//     }          
-    
-//     }
-//     console.log (output);
-//     }
 
 
 
@@ -311,4 +292,42 @@
 
 
 
-console.log ('pekla');
+
+// var output = [];
+
+// function fizzBuzz (){
+//     for (var count=1; count < 101; count++){
+//     if (count % 3 === 0 && count % 5 === 0) {
+//         output.push ('FizzBuzz');
+//     }
+//     else if (count % 3 === 0) {
+//         output.push ('Fizz');
+//     }
+//     else if (count % 5 === 0) {
+//         output.push ('buzz');
+//     }
+//     else {
+//         output.push (count);
+//     }          
+    
+//     }
+//     console.log (output);
+//     }
+
+
+function fibonacciGenerator (n) {
+    var output = [];
+    if (n===1) {
+       output = [0];
+    } else if (n===2) {
+       output = [0, 1];
+    } else {
+       output = [0, 1];
+       for (var i = 2; i < n; i++){
+       output.push(output[output.length - 2] + output[output.length - 1]);
+       }
+ }
+  return output;
+ }
+ output = fibonacciGenerator(5);
+ console.log (output);
